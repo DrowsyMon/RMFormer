5aaa40314ba67ef9db9fbbd149368f7e50aa5491
DrowsyMon <72968247+DrowsyMon@users.noreply.github.com>
2023-08-05 12:16:16 +0800

    Create __init__.py

---
+++ dataloader_collect/__init__.py
@@ -0,0 +1,3 @@
+# from .IDEA2 import IDEA2
+# from .data_loader_CV2 import RandomFlip, Rescale,RescaleT,RandomCrop,ToTensorLab,SalObjDataset
+from .data_loader_CV2_edge import RandomFlip, Rescale,RescaleT,RandomCrop,ToTensorLab,SalObjDataset
